197aff7909b34c01be8e16d7385b031106a0b84c
LiYanchen <fz20111018@gmail.com>
2019-06-02 20:51:25 +0800

    Create test.cpp

---
+++ SparseArray/test.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+#include"Sparse_Array.h"
+
+using namespace std;
+
+int main(){
+	auto vec1 = initial_2d_array(10,10);
+	print_2d_array(vec1);
+	
+  for(auto i = 0 ; i < 10 ; ++i){
+		vec[i][i] = i;
+	}
+	print_2d_array(vec1);
+	
+  auto vec2 = to_sparse_array(vec1);
+	print_2d_array(vec2);
+	
+  auto vec3 = to_normal_array(vec2);
+	print_2d_array(vec3);
+}
